3764c493d7c9e75721b2c4baf88c7f65b1c852fd
Daksh <dakshkaul7790@gmail.com>
2024-06-22 13:59:19 +0530

    Removed unnessary need for re-allocation for cli args, added license, and updated gitignore, removed execute lib from requirements

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use std::env;
+use std::process::Command;
+
+const TODO_FILE_PATH: &str = "/home/daksh/Documents/todo.org";
+
+fn get_command_line_args() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        eprintln!("Too many command-line arguments, check -h for manual page.");
+    } else if args.len() == 1 {
+        return "".to_string();
+    }
+
+    return args[1].clone();
+}
+
+fn edit_mode() {
+    let status = Command::new("nvim")
+        .arg(TODO_FILE_PATH)
+        .status()
+        .expect("Failed to execute edit command.");
+
+    if !status.success() {
+        eprintln!("nvim exited with an error.");
+    }
+}
+
+fn view_mode() {
+    let status = Command::new("bat")
+        .arg(TODO_FILE_PATH)
+        .status()
+        .expect("Failed to execute edit command.");
+
+    if !status.success() {
+        eprintln!("bat exited with an error.");
+    }
+}
+
+fn help_mode() {
+    let help_page: &str = r#"
+    NAME
+            todo
+
+    SYNOPSIS
+
+            View mode: todo
+            Edit mode: todo -e
+            Man  mode: todo -h
+
+    DESCRIPTION
+                
+            A command-line tool to help view, edit, and use your to-do files.
+
+    OPTIONS
+
+            -e
+                Opens to-do file in neovim.
+
+            -h
+                Outputs this help message.
+
+    AUTHOR
+                
+            Daksh Kaul // DriftingOtter
+    "#;
+
+    println!("{}", help_page);
+}
+
+fn main() {
+    match get_command_line_args().as_str() {
+        "-e" => edit_mode(),
+        "-h" => help_mode(),
+        ""   => view_mode(),
+        _    => eprintln!("Invalid command-line argument. Please use -h to open manual page."),
+    };
+}
